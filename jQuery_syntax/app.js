065ea7fad9d44b7943341cd22470549c9eb18655
marietrull <marietrull@gmail.com>
2018-11-10 10:14:38 -0600

    Added Syntax Excercise

---
+++ jQuery_syntax/app.js
@@ -0,0 +1,39 @@
+console.log("This is a jquery lesson")
+
+// to access (grab) an element, pass the css selector into the Jquery constructor
+// you will probably get an array-type thing if you're grabbing multiple elements,
+// and somethings when you're grabbing only one.
+
+//grab an element by id
+
+
+//grab an element by class
+
+
+//grab an element by tag name
+
+
+//create a div that is also a jQuery element
+
+
+//add text to your new Div. Comporable to .innerText property on a vanilla js element
+
+
+//append your new div to the body
+
+
+//create a p with jQuery with a class of "test-class"
+
+
+//use jQuery to apply css to the div: change the color of the div
+
+
+//Add a section to the page using jQuery. Give it a class "info"
+
+
+//Add a p to the section with some text. 
+
+
+//add a third image with an id of third-img
+
+//change the css of the container (give it a background color)
