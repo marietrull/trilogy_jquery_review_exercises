d92029cb0fa3b1cd016e98677b622fa1b41a879a
marietrull <marietrull@gmail.com>
2018-11-10 10:15:22 -0600

    Modified Syntax Excercise

---
+++ app.js
@@ -0,0 +1,36 @@
+console.log("This is a jquery lesson")
+
+//grab an element by id
+
+
+//grab an element by class
+
+
+//grab an element by tag name
+
+
+//create a div that is also a jQuery element
+
+
+//add text to your new Div. Comporable to .innerText property on a vanilla js element
+
+
+//append your new div to the body
+
+
+//create a p with jQuery with a class of "test-class"
+
+
+//use jQuery to apply css to the div: change the color of the div
+
+
+//Add a section to the page using jQuery. Give it a class "info"
+
+
+//Add a p to the section with some text. 
+
+
+//add a third image with an id of third-img
+
+
+//change the css of the container (give it a background color)
